939764a53e637077654a4a5c7394497b7b5705a2
Matthew Wall <matthewwall@users.noreply.github.com>
2020-05-10 15:32:11 -0400

    minor readme fix

--- install.py
+++ install.py
@@ -1,5 +1,5 @@
 # installer for influx
-# Copyright 2016 Matthew Wall
+# Copyright 2016-2020 Matthew Wall
 # Distributed under the terms of the GNU Public License (GPLv3)
 
 from weecfg.extension import ExtensionInstaller
